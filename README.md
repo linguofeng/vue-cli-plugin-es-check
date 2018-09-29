6d7b83da0e0e976e561290e05db80364829984c3
James Diacono <james@diacono.com.au>
2018-09-29 18:56:21 +1000

    🐣 release: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# @crosscountryapp/vue-cli-plugin-es-check
+
+Check built & bundled JS is strictly es5.  
+
+After build, call `vue-cli-service es-check`.
---
+++ index.js
@@ -0,0 +1,19 @@
+const shellExec = require('shell-exec')
+
+module.exports = (api, options) => {
+  api.registerCommand('es-check', {
+    description: 'check bundled js is es5',
+    usage: 'vue-cli-service es-check',
+  }, async () => {
+    const cmd = `es-check es5 ${options.outputDir}/js/*.js >&2`
+    const { code, stderr } = await shellExec(cmd)
+
+    if (code !== 0) {
+      throw new Error(stderr)
+    }
+  })
+}
+
+module.exports.defaultModes = {
+  'es-check': 'production'
+}
